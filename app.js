c4b0da4ad9a8d528137df40416fcddf5afad8a39
Rohit Sahu <sahu86744@gmail.com>
2024-11-27 23:33:35 +0530

    added cors

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-// app.js
 const express = require('express');
 const mongooseConnect = require('./config/db');
 const invoiceRouter = require('./routes/invoice');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors()); // add cors middleware
 
 // Connect to MongoDB
 mongooseConnect();
